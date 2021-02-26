cc5c287f101e29242d87403484ae8b425fb57b40
DESKTOP-9S501U7\MYLOCAL <timofey.gusev.opal@gmail.com>
2021-02-26 18:06:18 +0400

    галп

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+const {src,dest,watch, parallel} = require('gulp');
+const scss         = require('gulp-sass');
+const concat       = require('gulp-concat');
+var browserSync    = require('browser-sync').create();
+let uglify         = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
+
+function scripts(){
+    return src('app/js/main.js')
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(dest('app/'))
+        .pipe(browserSync.stream())
+}
+
+function build(){
+    return src([
+        'app/css/style.min.css',
+        'app/fonts/**/*',
+        'app/js/main.min.js',
+        'app/*.html',
+        'app/img/**/*'
+
+    ],{base: 'app'})
+        .pipe(dest('dist/'))
+}
+function browsersync(){
+    browserSync.init({
+        server: {
+            baseDir: "app/"
+        }
+    });
+}
+
+function styles(){
+    return src('app/scss/style.scss')
+        .pipe(scss())
+        .pipe(concat('style.min.css'))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 10 version']
+        }))
+        .pipe(dest('app/css'))
+        .pipe(browserSync.stream());
+}
+function watching(){
+    watch(['app/scss/**/*.scss'], styles);
+    watch(['img/**/*']);
+    watch(['app/js/main.js','!app/js/main.min.js'], scripts);
+    watch('app/index.html').on('change', browserSync.reload);
+}
+
+exports.styles = styles;
+exports.watching = watching;
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.build = build;
+exports.default = parallel( scripts,browsersync,watching);
